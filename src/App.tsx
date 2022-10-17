0281a2bb98a2e137b58916370f25151ba91fb5fa
Gabriel Gondim <52431033+GGVidal@users.noreply.github.com>
2022-10-17 17:01:04 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,13 @@ import { Header } from "./components";
 
 const App: FC = () => {
   const firebaseConfig = {
-    apiKey: "AIzaSyBhvRRyKPDAkvgDw4t0WtW7sELZ_03ozdo",
-    authDomain: "cartaoapi-63bb1.firebaseapp.com",
-    projectId: "cartaoapi-63bb1",
-    storageBucket: "cartaoapi-63bb1.appspot.com",
-    messagingSenderId: "334401711734",
-    appId: "1:334401711734:web:474a4c2980d666a7063957",
-    measurementId: "G-XPB02R9NC8",
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId,
   };
   const app = initializeApp(firebaseConfig);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
